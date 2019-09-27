9ad4743203de1a3618d5cd59bec13a2c99b93dcf
uttejh@gmail.com <uttejh@gmail.com>
2019-09-27 23:57:43 +0200

    upload data

--- main.py
+++ main.py
@@ -3,13 +3,29 @@ Sentiment Analysis with an RNN
 """
 
 
+def load_data(filepath):
+    """
+    reads a text file
+    :type filename: object
+    """
+    with open(filepath, "r") as f:
+        return f.read()
+
+
 def main():
     """
 
     :rtype: object
     """
 
-    # TODO: Load data
+    # get reviews
+    reviews = load_data("data/reviews.txt")
+    # get labels
+    labels = load_data("data/labels.txt")
+
+    # visualize data
+    print(reviews[:100])
+    print(labels[:10])
     # TODO: Visualize data
     # TODO: Data Pre-processing
     # TODO: Encoding
