888311e502a5ff0d42871396533fbf74cd778f96
uttejh@gmail.com <uttejh@gmail.com>
2019-09-27 19:54:23 +0200

    setup repo

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Sentiment Analysis with an RNN
+"""
+
+
+def main():
+    """
+
+    :rtype: object
+    """
+
+    # TODO: Load data
+    # TODO: Visualize data
+    # TODO: Data Pre-processing
+    # TODO: Encoding
+
+
+if __name__ == "__main__":
+    main()
