2f2faa01dc29c47aaf35539599a4823c2f7da8b6
uttejh@gmail.com <uttejh@gmail.com>
2019-09-28 17:07:44 +0200

    understanding beauty of git

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 """
 Sentiment Analysis with an RNN
 """
+from Preprocess import Preprocess
 
 
 def load_data(filepath):
     """
-    reads a text file
-    :type filename: object
+    reads contents of a file
+    Args:
+        filepath (str): The file location of the data
+
+    Returns:
+        str: The contents of the file
     """
     with open(filepath, "r") as f:
         return f.read()
@@ -26,10 +31,27 @@ def main():
     # visualize data
     print(reviews[:100])
     print(labels[:10])
-    # TODO: Visualize data
-    # TODO: Data Pre-processing
-    # TODO: Encoding
 
+    # class object
+    preprocess = Preprocess()
+
+    # get words
+    words, reviews_split = preprocess.get_words(reviews)
+
+    # map words to integers
+    vocab_to_int = preprocess.encode(words)
+
+    # tokenize reviews
+    review_ints = []
+    for review in reviews_split:
+        review_ints.append([vocab_to_int[word] for word in review.split()])
+
+    # visualize review integers
+    print('Unique words: ', len(vocab_to_int))
+    print(review_ints[:1])
+
+    # TODO: Remove outliers
+    # TODO: pad sequences with 0
 
 if __name__ == "__main__":
     main()
