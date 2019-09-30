02c15522c17372c4fe021e3758c35d6dbfa5c537
uttejh@gmail.com <uttejh@gmail.com>
2019-09-30 20:18:04 +0200

    Network in progress p2

--- Network.py
+++ Network.py
@@ -27,3 +27,32 @@ class Network(nn.Module):
         batch_size = x.size(0)
 
         x = x.long()
+        # embedding and lstm layers
+        embeds = self.embedding(x)
+        lstm_out, hidden = self.lstm(embeds, hidden)
+
+        # stack lstm outputs
+        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
+
+        # dropout and fully connected layer
+        out = self.dropout(lstm_out)
+        out = self.fc(out)
+
+        # sigmoid function
+        sig_out = self.sig(out)
+
+        # reshape to be batch_size first
+        sig_out = sig_out.view(batch_size, -1)
+        # get only last neuron output of each row
+        sig_out = sig_out[:, -1]
+
+        # return last sigmoid output and hidden state
+        return sig_out, hidden
+
+    def init_hidden(self, batch_size):
+        weight = next(self.parameters()).data
+
+        hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
+                  weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
+
+        return hidden
